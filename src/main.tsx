b4cba4585a38e1af2fe489db626e3c1723c0d9b8
vmpenyana <vincent@tenxrs.com>
2023-09-10 20:40:24 +0200

    Adding changes made to main.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Scanner from './components/camera/Scanner.tsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <div id='reader'></div>
+    <Scanner />
   </React.StrictMode>,
 )
