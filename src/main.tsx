6659fbd7ab2b165856104c5acb72eeb646afb214
vmpenyana <vincent@tenxrs.com>
2023-09-11 06:46:29 +0200

    removing unused variable

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Scanner from './components/camera/Scanner.tsx'
-import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
