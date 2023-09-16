394c802f78c44baa1923e65deede654918f2a065
vmpenyana <vincent@tenxrs.com>
2023-09-16 09:46:18 +0200

    Prototype: ticket invalidation on scan.

--- src/components/camera/Scanner.tsx
+++ src/components/camera/Scanner.tsx
@@ -1,19 +1,31 @@
 import { Html5Qrcode } from 'html5-qrcode';
+
+enum FACING_MODE {
+    ENV = "environment"
+}
+
+const handleScanSuccess = (decodeText:string, scanner:Html5Qrcode) => 
+{
+    scanner.stop();
+    console.log(decodeText)
+    fetch(`http://localhost:8080/invalidate/${decodeText}`).then(async (response) => {
+        const data = await response.json();
+        console.log(data);
+    }).catch(e => {
+        console.log("something went wrong: ", e)
+    })
+}
+
 const Scanner = () => {
     const getCameras = () => {
-    
-        const red = () => {
-            console.log("bad in business")
-        }
         Html5Qrcode.getCameras()
             .then(devices => {
                 if(devices && devices.length) {
                     const scanner = new Html5Qrcode("reader");
                     scanner.start({
-                        facingMode: "environment",
-                    }, {disableFlip: false, fps: 60}, (result:string) => {
-                        alert(`result: ${JSON.stringify(result)}`)
-                    }, red)
+                        facingMode: FACING_MODE.ENV,
+                    }, {disableFlip: false, fps: 60}, (decodedText) => handleScanSuccess(decodedText, scanner), 
+                    (e) => {})
                 }
             })
     }
